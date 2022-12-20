bffa6d1db196a374e2443003b849be33cc7d6f68
@pmnh <pmnhh1@gmail.com>
2022-12-20 11:03:35 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # synstats
 Used to pull a summary CSV of an SRT's statistics on the Synack Red Team platform
+
+# WARNING
+These statistics will by default contain the vulnerability title. Please ensure that you are appropriately securing information retrieved with this API and should be running it only on the LP+ platform.
+
+Please observe the rules for API throttling and feel free to modify the code appropriately to your needs, contributions in the form of pull requests are welcome.
+
+# Installation
+
+This library depends on the [synackAPI](https://github.com/gexpose/synackAPI) module, which is maintained by lvl0x0 and other key community members. This tool won't work unless the module is correctly installed and configured. We won't include installation instructions for the Synack API module here.
+
+# Usage
+
+Simply run the command as follows:
+
+`python -u synstats.py`
+
+Assuming you have your Synack Python API module correctly configured, you should see a CSV created with the following columns in it:
+
+```
+id	created_at	title	amount	category	subcategory	target	cvss	quality	created_at	resolved_at
+```
+
+The file will contain by default only `accepted` vulnerabilities. You are welcome to adjust the code to pull all your reported vulnerabilities if you would prefer.
+
---
+++ synstats.py
@@ -0,0 +1,45 @@
+from synack import synack
+from datetime import datetime
+from os import path
+import csv
+import os
+
+os.makedirs("vulns", exist_ok=True)
+
+s = synack()
+s.connectToPlatform()
+s.getSessionToken()
+
+vulns = s.getVulns("accepted")
+
+vulns_data = []
+count = 0
+for v in vulns:
+    if count % 50 == 0:
+        print("Analyzing %d of %d" % (count, len(vulns)))
+    count = count + 1
+
+    expanded_vuln = s.getVuln(v['id'])
+    
+    vulns_data.append({
+        "id": v['id'],
+        "title": v['title'],
+        # not sure what to do with timestamp format :)
+        "created_at": expanded_vuln['created_at'],
+        "resolved_at": expanded_vuln['resolved_at'],
+        "amount": v['market_value_final'],
+        "subcategory": v['category'],
+        "category": v['category_parent'],
+        "target": v['listing']['codename'],
+        "cvss": expanded_vuln['cvss_final'],
+        "quality": expanded_vuln['quality_score']
+    })
+
+
+columns = ["id", "created_at", "title", "amount", "category", "subcategory", "target", "cvss", "quality", "created_at", "resolved_at"]
+now = datetime.now()
+filename = "synstats-%s-%s-%s.csv"%(str(now.year),str(now.month),str(now.day))
+with open(filename,"w") as f:
+    writer = csv.DictWriter(f, fieldnames=columns, extrasaction="ignore", lineterminator="\n")
+    writer.writeheader()
+    writer.writerows(vulns_data)
